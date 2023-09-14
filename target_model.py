c45cb94d08529fe228bacef2c212ef39bfad694e
zeyangsha <116175544+zeyangsha@users.noreply.github.com>
2023-09-14 12:43:55 +0200

    Update target_model.py

--- target_model.py
+++ target_model.py
@@ -7,8 +7,7 @@ import torchvision
 class target_model(nn.Module):
     def __init__(self,out_dim):
         super(target_model,self).__init__()
-        self.encoder = torchvision.models.resnet18(pretrained=False,num_classes=512)
-
+        self.encoder = torchvision.models.resnet18(pretrained=False,num_classes=512
         self.out_dim = out_dim
         self.linear = nn.Linear(self.encoder.fc.out_features,self.out_dim)
 
